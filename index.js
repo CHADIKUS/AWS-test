4bfac0cd925646aeb90cd40fb9023f5066d0fe9e
battlebrotherchad-git <69522892+battlebrotherchad-git@users.noreply.github.com>
2022-09-03 23:40:55 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -2,12 +2,50 @@ const express = require('express')
 const fs = require('fs');
 var bodyParser = require("body-parser");
 const app = express();
+
+app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 const port = 8080;
 
 app.get('/', (req, res) => {
-    res.send("AAAAAAAAAAAAUGH")
+    fs.readFile('public/index.html', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        res.send(data);
+        });
+})
+
+app.get('/northstar', (req, res) => {
+    fs.readFile('public/northstar.html', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        res.send(data);
+        });
+})
+
+app.get('/cube', (req, res) => {
+    fs.readFile('public/cube.html', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        res.send(data);
+        });
+})
+
+app.get('/neon', (req, res) => {
+    fs.readFile('public/neon.html', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        res.send(data);
+        });
 })
 
 app.listen(port, () => {
